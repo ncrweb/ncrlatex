02075fe4f8914dce71278b2a107b8d2f50e3bdd8
Koen Berends <k.d.berends@utwente.nl>
2022-04-12 10:37:47 +0200

    ncrlatex #5 fixed bash syntax

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Find the texmf root directory 
-tex_root=kpsewhich -var-value=TEXMFHOME
+tex_root=$(kpsewhich -var-value=TEXMFHOME)
+echo "Installing package to TEXMFHOME: ${tex_root}"
 
 # copy ncrlatex to tex
-cp -r ./ncrlatex/ncrlatex tex_root
+cp -r ./ncrlatex "${tex_root}"
 
 # finish
 echo "..ncrlatex installed"
